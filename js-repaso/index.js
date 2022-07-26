0e9a9fd3ce337e4fa56adfd0d4f67eeebdc9fee0
Dario Diaz <dariodiaz1595@gmail.com>
2022-07-26 10:59:07 -0300

    update

---
+++ js-repaso/index.js
@@ -0,0 +1,7 @@
+var miVariable = 'variable'; // Var palabra reservada, el comilador por detras, va al inicio del archivor y declara la variable 
+
+let variable = 'variable creada con let'; // Es similar a var, ya que se puede reasignar el valor de la variable
+// let es creada en la misma linea, a diferencia de var que se declara al inicio
+
+const constante = 'este valor es constante'; 
+// A las const no se le puede reasignar un valor
